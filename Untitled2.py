ee6b370ac1147c434ad4a3893748a36483101e4e
hugowellens <114393623+hugowellens@users.noreply.github.com>
2022-09-28 13:49:34 +0200

    Add files via upload

---
+++ Untitled2.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# In[3]:
+
+
+#datasets inladen
+netflix = pd.read_csv('NetflixOriginals.csv')
+netflix_extra = pd.read_csv('netflix_original_movie_data.csv')
+amazon_prime = pd.read_csv('amazon prime movies.csv')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
